d2346a539e42cb155a7b5f429aee94d5e0bc7ad6
Pepijn de Vos <pepijndevos@gmail.com>
2011-03-15 00:06:41 +0100

    sudoku should be working, but I can do it faster by hand. Note that no real board has been finished yet.

--- src/logos/sudoku.clj
+++ src/logos/sudoku.clj
@@ -1,7 +1,9 @@
 (ns logos.sudoku
-  (:refer-clojure :exclude [reify ==])
-  (:use logos.minikanren
-        logos.logic))
+  (:refer-clojure :exclude [reify == inc])
+  (:use [logos
+           minikanren
+           logic
+           nonrel]))
 
 (def board [(lvar) 7      (lvar) (lvar) (lvar) 8      (lvar) (lvar) 5
             (lvar) (lvar) 8      3      (lvar) 1      (lvar) (lvar) (lvar)
@@ -13,6 +15,16 @@
             (lvar) (lvar) (lvar) 4      (lvar) 7      9      (lvar) (lvar)
             7      (lvar) (lvar) 8      (lvar) (lvar) (lvar) 6      (lvar)])
 
+(def board2 [9      4      2      5      3      7      6      1      8
+             (lvar) 7      (lvar) (lvar) 8      6      9      4      3
+             (lvar) 3      8      (lvar) (lvar) 1      2      7      5
+             3      (lvar) 7      6      1      (lvar) 4      (lvar) (lvar)
+             4      (lvar) (lvar) (lvar) 5      (lvar) 7      3      6
+             (lvar) 2      6      3      (lvar) 4      1      (lvar) 9
+             8      6      (lvar) (lvar) (lvar) 5      8      2      (lvar)
+             (lvar) (lvar) 9      4      2      8      3      (lvar) 7
+             2      8      (lvar) 7      (lvar) 3      5      9      1])
+
 (def full-board [5 3 4 6 7 8 9 1 2
                  6 7 2 1 9 5 3 4 8
                  1 9 8 3 4 2 5 6 7
@@ -23,40 +35,34 @@
                  2 8 7 4 1 9 6 3 5
                  3 4 5 2 8 6 1 7 9])
 
+(def almost-board [5 3 4 6 7 8 9 1 (lvar)
+                   6 7 2 1 9 5 3 4 (lvar)
+                   1 9 8 3 4 2 5 6 (lvar)
+                   8 5 9 7 6 1 4 2 (lvar)
+                   4 2 6 8 5 3 7 9 (lvar)
+                   7 1 3 9 2 4 8 5 (lvar)
+                   9 6 1 5 3 7 2 8 (lvar)
+                   2 8 7 4 1 9 6 3 (lvar)
+                   3 4 5 2 8 6 1 7 (lvar)])
+
 (def empty-board (repeatedly 81 lvar))
 
-(defn digit-o [d]
+(defn remember-o [x l out]
   (cond-e
-    ((== 1 d))
-    ((== 2 d))
-    ((== 3 d))
-    ((== 4 d))
-    ((== 5 d))
-    ((== 6 d))
-    ((== 7 d))
-    ((== 8 d))
-    ((== 9 d))))
-
-(defn digit-e [b]
-  (cond-e
-    ((== b []))
-    ((exist [x y]
-       (cons-o x y b)
-       (digit-o x)
-       (digit-e y)))))
-
-(defn board-o [b]
-  (all
-    (== b (repeatedly 81 lvar))
-    (digit-e b)))
+   ((cons-o x out l))
+   ((exist [f r i]
+           (cons-o f r l)
+           (cons-o f i out)
+           (remember-o x r i)))))
 
 (defn permutation-o [s l]
   (cond-e
     ((== s []) (== l []))
-    ((exist [x y z]
+    ((exist [a b x y z]
+       (cons-o a b s)
        (cons-o x y l)
-       (rember-o x l z)
-       (permutation-o y z)))))
+       (remember-o x s z)
+       (permutation-o z y)))))
 
 (defn slice [board]
   (let [rows (partition 9 board)
@@ -77,7 +83,6 @@
         [rows cols blocks] (slice board)]
     (concat
       `(exist [~@board]
-              (board-o ~b)
               (== ~b (list ~@board)))
       (map (fn [row]
              `(permutation-o (list ~@row) (range 1 10)))
